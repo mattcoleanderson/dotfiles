6b3457720cce9a52b740b76018c1bd235b47ccbb
mattcoleanderson <github@mattcoleanderson.com>
2023-11-28 07:33:05 -0600

    Add setup script for zsh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,23 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Source Files
 
 # TODO: This script will create a fresh environment when ran. Needs to be written
+source ./zsh/zshenv
+
+cat << "EOF"
+         88                               
+         88              ,d               
+         88              88               
+ ,adPPYb,88  ,adPPYba, MM88MMM ,adPPYba,  
+a8"    `Y88 a8"     "8a  88    I8[    ""  
+8b       88 8b       d8  88     `"Y8ba,   
+"8a,   ,d88 "8a,   ,a8"  88,   aa    ]8I  
+ `"8bbdP"Y8  `"YbbdP"'   "Y888 `"YbbdP"'
+EOF
+
+# Setup
+
+. "$DOTFILES/install/install-zsh.sh"
+
+
