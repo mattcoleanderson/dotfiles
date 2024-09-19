7a3c69875ab89cdc8ba6cad05953058501528a5e
mattcoleanderson <github@mattcoleanderson.com>
2024-09-19 11:42:35 -0500

    add homebrew to zsh completion FPATh

--- homebrew/path.zsh
+++ homebrew/path.zsh
@@ -1,1 +1,3 @@
 PATH="$HOMEBREW_PREFIX/bin:$HOMEBREW_PREFIX/sbin:$PATH" # Homebrew [7]
+FPATH="$HOMEBREW_PREFIX/share/zsh-completions:$FPATH"
+FPATH="$HOMEBREW_PREFIX/share/zsh/site-functions:$FPATH"
