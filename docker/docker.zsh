51af89d16ece60526d3ddd26132a1b6a2e3211c3
Matthew Anderson <matt.anderson@Matts-MacBook-Pro.local>
2025-01-15 23:28:35 -0600

    comment out testcontainer related docker configs

--- docker/docker.zsh
+++ docker/docker.zsh
@@ -1,10 +1,13 @@
+# Set docker config to XDG_CONFIG_HOME
+export DOCKER_CONFIG="$XDG_CONFIG_HOME/docker"
+
 # DOCKER_HOST sets the socket that docker engine is connected to
 # Currently I am using colima as my company computer is a mac and I am asked to not use Docker Hub
 # so below is an explicit setting to colima as my engine
-export DOCKER_HOST="unix:///Users/mattcoleanderson/.local/config/colima/default/docker.sock"
+# export DOCKER_HOST="unix:///Users/mattcoleanderson/.local/config/colima/default/docker.sock"
 # export DOCKER_HOST="unix://${XDG_CONFIG_HOME}/.colima/default/docker.sock"
-export TESTCONTAINERS_DOCKER_SOCKET_OVERRIDE=/var/run/docker.sock
+
+# Used for testcontainers, a testing suite for testing in docker containers
+# export TESTCONTAINERS_DOCKER_SOCKET_OVERRIDE=/var/run/docker.sock
 # export TESTCONTAINERS_HOST_OVERRIDE=$(colima ls -j | jq -r '.address')
-export TESTCONTAINERS_RYUK_DISABLED=true
-# Set docker config to XDG_CONFIG_HOME
-export DOCKER_CONFIG="$XDG_CONFIG_HOME/docker"
+# export TESTCONTAINERS_RYUK_DISABLED=true
